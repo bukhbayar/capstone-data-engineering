f94b3a3a1e40f56fc9be3830422fcad1cbcb8c74
Buku <bokhbayar@gmail.com>
2025-08-26 20:14:31 +1000

    update scripts

--- terraform/main.tf
+++ terraform/main.tf
@@ -215,7 +215,7 @@ module "ec2-airflow" {
     systemctl start airflow-worker
 
 
-    sudo -u airflow aws s3 sync s3://${module.code_bucket.name}/dags/ /home/airflow/airflow/dags --delete
+    sudo -u airflow aws s3 sync s3://${module.code_bucket.bucket_name}/dags/ /home/airflow/airflow/dags --delete
 
   EOF
 }
