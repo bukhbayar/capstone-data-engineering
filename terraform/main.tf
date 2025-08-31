c8b98d0a90d8776f3ddaf02309d6a18b5d2aec57
Buku <bokhbayar@gmail.com>
2025-08-31 17:38:49 +1000

    updating export script

--- terraform/main.tf
+++ terraform/main.tf
@@ -215,13 +215,6 @@ module "ec2-airflow" {
     sleep 5
     systemctl start airflow-worker
 
-
-    # Create postgress connection
-    su - airflow -c "set -a; source /etc/airflow/airflow.env; set +a; source ~/venv/bin/activate; \
-    airflow connections add postgres_default \
-    --conn-uri "postgresql+psycopg2://bootcamp_user:bootcamp_password@10.20.1.50:5432/bootcamp_db" \
-    --conn-description "Postgres on EC2 (bootcamp_db)"
-
     sudo -u airflow aws s3 sync s3://${module.code_bucket.bucket_name}/dags/ /home/airflow/airflow/dags --delete
 
   EOF
