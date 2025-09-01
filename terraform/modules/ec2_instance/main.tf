3e1bc8a42b10b1f0b59ba9d878cf7d34dbe535f1
Buku <bokhbayar@gmail.com>
2025-09-01 19:38:42 +1000

    updating export script

--- terraform/modules/ec2_instance/main.tf
+++ terraform/modules/ec2_instance/main.tf
@@ -18,19 +18,45 @@ resource "aws_instance" "this" {
   }
 }
 
-resource "null_resource" "instance" {
+resource "terraform_data" "provision_every_apply" {
   count = var.airflow_scripts!="" ? 1 : 0
+  # keep this tied to your instance so if it’s replaced, this re-runs too
+  input = {
+    instance_id = aws_instance.this.id
+  }
 
-  provisioner "local-exec" {
-    connection {
-      type        = "ssh"
-      user        = "ec2-user"
-      private_key = file(var.ssh_private_key)
-      host        = aws_instance.this.public_ip
-    }
+  # the magic: any change here forces replacement → provisioners run again
+  triggers_replace = timestamp()
 
-    command = var.airflow_scripts
+  connection {
+    host        = aws_instance.this.private_ip
+    user        = "ec2-user"          # or "ubuntu", etc
+    private_key = var.ssh_private_key
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo 'Hello from remote-exec at $(date)' | sudo tee /tmp/hello.txt",
+      "${var.airflow_scripts}"
+    ]
   }
 
   depends_on = [aws_instance.this]
 }
+
+# resource "null_resource" "instance" {
+#   count = var.airflow_scripts!="" ? 1 : 0
+
+#   provisioner "local-exec" {
+#     connection {
+#       type        = "ssh"
+#       user        = "ec2-user"
+#       private_key = var.ssh_private_key
+#       host        = aws_instance.this.public_ip
+#     }
+
+#     command = var.airflow_scripts
+#   }
+
+#   depends_on = [aws_instance.this]
+# }
