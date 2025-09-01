d1a3abb73c3773256835176414eeb62c87b037bc
Buku <bokhbayar@gmail.com>
2025-09-02 00:17:41 +1000

    updating export script

--- scripts/batch_dag.py
+++ scripts/batch_dag.py
@@ -1,40 +1,56 @@
 from datetime import datetime
 from airflow import DAG
 from airflow.models import Variable
-from airflow.utils.trigger_rule import TriggerRule
+
 from airflow.providers.amazon.aws.operators.batch import BatchOperator
+from airflow.providers.amazon.aws.sensors.batch import BatchSensor
 
-
-JOB_QUEUE       = Variable.get("BATCH_JOB_QUEUE", default_var="data-lake-dev-batch-queue")
-JOB_DEFINITION  = Variable.get("BATCH_JOB_DEFINITION", default_var="data-lake-dev-batch-dbt")
-DEFAULT_TARGET  = Variable.get("DBT_TARGET", default_var="dev")
+AWS_CONN_ID    = Variable.get("AWS_CONN_ID", default_var="aws_default")
+AWS_REGION     = Variable.get("AWS_REGION",   default_var="ap-southeast-2")
+JOB_QUEUE      = Variable.get("BATCH_JOB_QUEUE",      default_var="data-lake-dev-batch-queue")
+JOB_DEFINITION = Variable.get("BATCH_JOB_DEFINITION", default_var="data-lake-dev-batch-dbt")
+DEFAULT_TARGET = Variable.get("DBT_TARGET",           default_var="dev")
 
 with DAG(
     dag_id="dbt_batch_runner",
     start_date=datetime(2025, 1, 1),
-    schedule=None,   # trigger manually or via another DAG
+    schedule=None,
     catchup=False,
-    params={
-        # default cmd; can be overridden in Trigger DAG -> JSON config
-        "dbt_cmd": f"run -t {DEFAULT_TARGET}",
-    },
-    tags=["dbt","batch"],
+    params={"dbt_cmd": f"dbt run -t {DEFAULT_TARGET}"},
+    tags=["dbt", "batch"],
 ) as dag:
 
-    run_dbt = BatchOperator(
+    # Submit the job
+    submit = BatchOperator(
         task_id="submit_dbt_job",
-        job_name="dbt-{{ ds_nodash }}",
+        job_name="dbt-{{ ts_nodash }}",
         job_queue=JOB_QUEUE,
         job_definition=JOB_DEFINITION,
-        # Rest of the configuration remains the same
+
+        # If your Job Definition uses command: ["bash","-lc","Ref::cmd"]
         parameters={
             "cmd": "{{ dag_run.conf.get('dbt_cmd', params.dbt_cmd) }}"
         },
-        overrides={
-            "environment": [
-                {"name": "DBT_TARGET", "value": f"{DEFAULT_TARGET}"},
-            ]
-        },
-        wait_for_completion=True,
-        max_retries=0,
+
+        # If your Job Definition does NOT use parameters/Ref::cmd, instead use this:
+        # overrides={
+        #   "command": ["bash","-lc","{{ dag_run.conf.get('dbt_cmd', params.dbt_cmd) }}"],
+        #   "environment": [{"name": "DBT_TARGET", "value": DEFAULT_TARGET}],
+        # },
+
+        aws_conn_id=AWS_CONN_ID,
+        region_name=AWS_REGION,
     )
+
+    # Wait for completion (BatchOperator in some versions doesn't block)
+    wait = BatchSensor(
+        task_id="wait_for_dbt",
+        job_id=submit.output,            # XCom from submit task
+        aws_conn_id=AWS_CONN_ID,
+        region_name=AWS_REGION,
+        poke_interval=30,                # seconds
+        timeout=60 * 60 * 3,             # 3 hours
+        mode="poke",                     # or "reschedule" if you prefer
+    )
+
+    submit >> wait
