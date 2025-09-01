def718a8eef4237d9122198c267dd5960ddf7d73
Buku <bokhbayar@gmail.com>
2025-09-01 23:30:24 +1000

    updating export script

--- terraform/modules/ec2_instance/local.tf
+++ terraform/modules/ec2_instance/local.tf
@@ -16,12 +16,12 @@ locals {
     PG_URI="postgresql+psycopg2://$${POSTGRES_USER}:$${POSTGRES_PASSWORD}@$${POSTGRES_HOST}:$${POSTGRES_PORT}/$${POSTGRES_DB}"
 
     # Idempotent upsert of the connection
-    if su - airflow -c "source ~/venv/bin/activate; airflow connections get postgres_default >/dev/null 2>&1"; then
+    if sudo su - airflow -c "source ~/venv/bin/activate; airflow connections get postgres_default >/dev/null 2>&1"; then
       echo "[conn:postgres_default] exists → overwriting"
-      su - airflow -c "source ~/venv/bin/activate; airflow connections add postgres_default --overwrite --conn-uri '$${PG_URI}' --conn-description 'Postgres on EC2 - bootcamp_db'"
+      sudo su - airflow -c "source ~/venv/bin/activate; airflow connections add postgres_default --overwrite --conn-uri '$${PG_URI}' --conn-description 'Postgres on EC2 - bootcamp_db'"
     else
       echo "[conn:postgres_default] creating"
-      su - airflow -c "source ~/venv/bin/activate; airflow connections add postgres_default --conn-uri '$${PG_URI}' --conn-description 'Postgres on EC2 - bootcamp_db'"
+      sudo su - airflow -c "source ~/venv/bin/activate; airflow connections add postgres_default --conn-uri '$${PG_URI}' --conn-description 'Postgres on EC2 - bootcamp_db'"
     fi
 
     echo "Airflow connections ensured."
