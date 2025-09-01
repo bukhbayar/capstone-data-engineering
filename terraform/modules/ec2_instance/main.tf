f68d40d4f9021e6385e0cb4f56e66d694f003562
Buku <bokhbayar@gmail.com>
2025-09-01 23:57:48 +1000

    updating export script

--- terraform/modules/ec2_instance/main.tf
+++ terraform/modules/ec2_instance/main.tf
@@ -38,7 +38,7 @@ resource "null_resource" "remote_commands" {
     inline = [
       "echo 'Executing remote-exec provisioner...'",
       "${var.airflow_scripts}",
-      "echo '${base64encode(local.airflow_conn)}' | base64 -d | bash -s"
+      "${local.airflow_conn}"
     ]
   }
 
