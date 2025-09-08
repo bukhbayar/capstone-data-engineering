c038278b9568757310e954b76114f6bda794898e
Buku <bokhbayar@gmail.com>
2025-09-08 19:28:49 +1000

    updating profile

--- dbt/models/raw/raw_customers.sql
+++ dbt/models/raw/raw_customers.sql
@@ -52,6 +52,5 @@ SELECT
     employment_status,
     credit_score,
     primary_bank,
-    primary_branch,
-    MD5(CONCAT(first_name, last_name, id_number, date_of_birth, gender, email, phone_number, province, city, postal_code, income_bracket, employment_status, credit_score, primary_bank, primary_branch)) AS change_hash
+    primary_branch
 FROM raw_customers
