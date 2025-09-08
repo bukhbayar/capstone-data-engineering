be330da0d7a84a03a0950fc33e84769ebd822331
Buku <bokhbayar@gmail.com>
2025-09-08 19:49:55 +1000

    updating iam in batch

--- terraform/modules/batch/iam.tf
+++ terraform/modules/batch/iam.tf
@@ -60,6 +60,11 @@ resource "aws_iam_role_policy_attachment" "batch_attach_athena" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonAthenaFullAccess"
 }
 
+resource "aws_iam_role_policy_attachment" "batch_attach_s3" {
+  role       = aws_iam_role.task_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
 # Logs
 resource "aws_cloudwatch_log_group" "batch" {
   name              = "/aws/batch/${local.name}"
