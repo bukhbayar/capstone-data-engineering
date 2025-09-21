295eb8b3606c104d6adeee969f5b375bed73e7ef
Buku <bokhbayar@gmail.com>
2025-09-21 20:43:26 +1000

    updating lambda

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,7 +1,8 @@
 
 locals {
   name = "${var.project}-${var.environment}"
-  yt_api_key = data.aws_secretsmanager_secret_version.yt_latest.secret_string
+  yt_api_key = aws_secretsmanager_secret_version.youtube_secret.secret_string
+  # data.aws_secretsmanager_secret_version.yt_latest.secret_string
 
   db_bootstrap_sql = join("\n\n", [
     for db in var.databases : <<-SQL
